5822f4e64b220f9ba5c6cd970b17902ea0c00d90
Amaan Vora <43636138+deadven7@users.noreply.github.com>
2021-11-19 12:12:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 80);
+});
+
+$(document).ready(function(){
+    $(".navbar a, footer a[href='#index']").on('click', function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 900, function(){
+            window.location.hash = hash;
+            });
+        }
+    });
+})
+
+$(window).scroll(function() {
+    $(".slideanim").each(function(){
+        var pos = $(this).offset().top;
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 750) {
+            $(this).addClass("slide");
+        }
+    });
+});
+
+$('.counter-count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
